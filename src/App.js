484eb9ca3b81f7d420a88a17621e3c4e20000161
Vikarn <dharm.surfing@gmail.com>
2022-01-04 21:42:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
